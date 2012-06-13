01e4488dc521b2dae2b90b91390db2fc7224233b
isbaysoft <isbaysoft@gmail.com>
2012-06-13 18:26:04 +0300

    added JPG to extension_white_list document uploader

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -12,7 +12,7 @@ class DocumentUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(txt doc docx xls xlsx pdf)
+    %w(txt doc docx xls xlsx pdf jpg)
   end
 
   # Override the filename of the uploaded files:
