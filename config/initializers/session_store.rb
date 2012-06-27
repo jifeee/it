e206ec6da85ca688bea03e8f45b4f3082894b085
isbaysoft <isbaysoft@gmail.com>
2012-06-27 23:47:56 +0300

    try to fight with chrome session bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
-# Instatrace::Application.config.session_store :cookie_store, :key => '_instatrace_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
- Instatrace::Application.config.session_store :active_record_store
 
+# Instatrace::Application.config.session_store :active_record_store
+
+
+Instatrace::Application.config.session_store :cookie_store
 Instatrace::Application.config.action_dispatch.session = {
   :key    => '_instatrace_session',
   :secret => '0x0dkfj3927dkc7djdh36rkckdfzsg'
