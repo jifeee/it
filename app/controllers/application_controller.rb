727e9c92228eb47fc22bf31ae2df8463b97ca192
isbaysoft <isbaysoft@gmail.com>
2012-06-27 14:14:37 +0300

    fix shipment search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.0.8'
+  APPLICATION_VERSION = '1.0.9'
 
   protect_from_forgery
 
