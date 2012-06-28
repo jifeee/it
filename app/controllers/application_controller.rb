62624221aa8f5e03f7628d2a8ba006a8a2bbf0bd
isbaysoft <isbaysoft@gmail.com>
2012-06-28 14:47:44 +0300

    link shipment by hawb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.0.12'
+  APPLICATION_VERSION = '1.0.14'
 
   protect_from_forgery
 
--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -2,22 +2,20 @@ class ShipmentsController < ApplicationController
   # load_and_authorize_resource :except => :show
   
   def index
-    session['user_shipment_ids'] = nil
     @hawbs = Shipment.all.map &:hawb
     @shipments = Shipment.search(params[:shipment]).page(params[:page]).per(20)
     @search = Shipment.new(params[:shipment])
-    # session['user_shipment_ids'] = @shipments.all.map {|s| s.id.to_i}.join(',')
-    session['user_shipment_ids'] = @shipments.all.map &:id
   end
 
   def show
-    @shipment = Shipment.find(params[:id])
+    @shipment = Shipment.find_by_hawb(params[:id])
+    # @shipment = Shipment.find(params[:id])
     # if (current_user && !current_user.sa? && (!current_user.allowed_shipments.include?(params[:id].to_i) || !session[:user_shipment_ids].include?(params[:id].to_i))) || 
     #   ((current_user.nil? && session[:user_shipment_ids].nil?) || (current_user.nil? && !session[:user_shipment_ids].include?(params[:id].to_i)))
     #   redirect_to shipments_path 
     # end
-    ids = session['user_shipment_ids']
-    redirect_to shipments_path, :notice => session['user_shipment_ids'].join(',') unless session['user_shipment_ids'].include?(params[:id].to_i)
+    # ids = session['user_shipment_ids']
+    # redirect_to shipments_path, :notice => session['user_shipment_ids'].join(',') unless session['user_shipment_ids'].include?(params[:id].to_i)
   rescue
     redirect_to root_path
   end
