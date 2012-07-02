2481e234ba1ce252030f35fbaec1244a59ab0473
isbaysoft <isbaysoft@gmail.com>
2012-07-02 10:12:27 +0300

    session fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.0.14'
+  APPLICATION_VERSION = '1.0.15'
 
   protect_from_forgery
 
