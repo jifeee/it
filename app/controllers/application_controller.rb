f59c0e93c139751a83d0b13bc9746a1ac5e999b1
isbaysoft <isbaysoft@gmail.com>
2012-07-02 14:05:53 +0300

    fix current_user declaration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.0.16'
+  APPLICATION_VERSION = '1.0.17'
 
   protect_from_forgery
 
