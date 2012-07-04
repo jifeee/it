24fbf8a6cdcc222ebe76d6a57f19a51dea797c1c
isbaysoft <isbaysoft@gmail.com>
2012-07-04 12:50:44 +0300

    fix redirect shipment_path - need hawb instead of id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.0.18'
+  APPLICATION_VERSION = '1.0.19'
 
   protect_from_forgery
 
