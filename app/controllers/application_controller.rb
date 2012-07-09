38fd503ac150025c445f6ed50fc77907d10008a6
isbaysoft <isbaysoft@gmail.com>
2012-07-09 10:53:57 +0300

    1.0.20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # module I18n; def self.t(id); '~'; end; end
 
 class ApplicationController < ActionController::Base
-  APPLICATION_VERSION = '1.0.19'
+  APPLICATION_VERSION = '1.0.20'
 
   protect_from_forgery
 
