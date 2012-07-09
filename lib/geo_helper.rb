285b57d43945e40cf598cbcd0b5b9d31bb679228
isbaysoft <isbaysoft@gmail.com>
2012-07-09 19:13:17 +0300

    x

--- lib/geo_helper.rb
+++ lib/geo_helper.rb
@@ -3,6 +3,6 @@ require 'rest_client'
 module GeoHelper
 	def timeshift
 		zone = RestClient.get "http://api.geonames.org/timezone?lat=#{latitude}&lng=#{longitude}&username=instatrace"
-		Hash.from_xml(zone)["geonames"]["timezone"]["gmtOffset"].to_f rescue nil
+		Hash.from_xml(zone)["geonames"]["timezone"]["gmtOffset"].to_f
 	end
 end
