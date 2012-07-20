7714daa817ba849c8e8a54e0e604f573579a5f2b
isbaysoft <isbaysoft@gmail.com>
2012-07-20 18:41:21 +0300

    git_deploy

--- git_deploy.sh
+++ git_deploy.sh
@@ -9,7 +9,7 @@ kill -9 `cat tmp/pids/unicorn.pid`
 sleep 5
 kill -9 `cat tmp/pids/unicorn.pid`
 sleep 2
-RAILS_ENV=production bundle exec unicorn_rails -o 127.0.0.1 -c config/unicorn.rb -E production -D
+RAILS_ENV=production bundle exec unicorn_rails -o 127.0.0.1 -c config/unicorn.rb -E production -D -p 3001
 sleep 4
 RAILS_ENV=production script/delayed_job start
 
